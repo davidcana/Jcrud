fb1fb6763fb83c82446fa81cac28068781d18ae7
David Cana Lopez <david.javapagetemplates@gmail.com>
2019-02-11 19:39:34 +0100

    bug fixed: now GenericZCrudResponse class implements ZCrudResponse.

--- src/main/java/org/github/davidcana/jcrud/core/responses/GenericZCrudResponse.java
+++ src/main/java/org/github/davidcana/jcrud/core/responses/GenericZCrudResponse.java
@@ -2,11 +2,10 @@ package org.github.davidcana.jcrud.core.responses;
 
 import org.github.davidcana.jcrud.core.ZCrudEntity;
 
-abstract public class GenericZCrudResponse<T extends ZCrudEntity> extends AbstractZCrudResponse {
+abstract public class GenericZCrudResponse<T extends ZCrudEntity> extends AbstractZCrudResponse implements ZCrudResponse {
 
 	@Override
 	public String toString() {
 		return "GenericZCrudResponse []";
 	}
-	
 }
