654a86b2fd998ec23c15e3df23ddbe13f443acb6
David Cana <david.javapagetemplates@gmail.com>
2019-03-05 11:59:07 +0100

    AbstractZCrudRequestTest improved: new file generated when unexpected results are found.

---
+++ src/test/java/org/github/davidcana/jcrud/core/ObjectMapperProviderForTest.java
@@ -0,0 +1,55 @@
+package org.github.davidcana.jcrud.core;
+
+import java.io.IOException;
+
+import org.github.davidcana.jcrud.core.requests.ZCrudRequest;
+import org.github.davidcana.jcrud.storages.Storage;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ObjectMapperProviderForTest {
+	
+	static private ObjectMapperProviderForTest instance;
+	
+	private ObjectMapper mapper;
+	
+	public ObjectMapperProviderForTest(){}
+	
+	public ObjectMapper get(){
+		
+		if (this.mapper == null){
+			this.mapper = instanceObjectMapper();
+		}
+		
+		return this.mapper;
+	}
+
+	public ZCrudRequest getZCrudRequest(String json, Storage storage) throws IOException, JsonParseException, JsonMappingException {
+		
+		JavaType type = this.get().getTypeFactory().constructParametricType(ZCrudRequest.class, storage.getDeserializeClass());
+		return (ZCrudRequest) this.get().readValue(json, type);
+	}
+	
+	static private ObjectMapper instanceObjectMapper(){
+		
+		ObjectMapper mapper = ObjectMapperProvider.instanceObjectMapper();
+		
+		//mapper.enableDefaultTyping();
+		//mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		
+		return mapper;
+	}
+	
+	static public ObjectMapperProviderForTest getInstance(){
+		
+		if ( instance == null ){
+			instance = new ObjectMapperProviderForTest();
+		}
+		
+		return instance;
+	}
+
+}
--- src/test/java/org/github/davidcana/jcrud/core/requests/AbstractZCrudRequestTest.java
+++ src/test/java/org/github/davidcana/jcrud/core/requests/AbstractZCrudRequestTest.java
@@ -3,11 +3,17 @@ package org.github.davidcana.jcrud.core.requests;
 import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.StringReader;
+import java.io.Writer;
+import java.net.URL;
 
 import org.github.davidcana.jcrud.core.DefaultCRUDManager;
+import org.github.davidcana.jcrud.core.ObjectMapperProviderForTest;
 import org.github.davidcana.jcrud.core.requests.ZCrudRequest;
 import org.github.davidcana.jcrud.storages.SimpleVoidStorage;
 import org.junit.After;
@@ -19,6 +25,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 abstract public class AbstractZCrudRequestTest {
 	
 	public static final String TESTS_PATH = "/requests/";
+	private static final String NEW_FILE_SUFFIX = ".new";
 	
 	@Before
 	public void setUp() throws Exception {
@@ -39,7 +46,12 @@ abstract public class AbstractZCrudRequestTest {
         		SimpleVoidStorage.getInstance()
         );
         
-        assertEquals("failure - strings are not equal", expected, zCrudRequest);
+        //assertEquals("failure - strings are not equal", expected, zCrudRequest);
+		if (! expected.equals(zCrudRequest)){
+		    fail(
+		    		"unexpected results: see " + this.saveNew(test, zCrudRequest)
+		    );
+		}
 
         long elapsed = System.currentTimeMillis() - start;
         System.err.println(test + ": tested in " + elapsed + " ms");
@@ -54,5 +66,60 @@ abstract public class AbstractZCrudRequestTest {
         		)
         );
 	}
-
+	
+	protected String saveNew(String test, ZCrudRequest zCrudRequest) throws IOException {
+		
+        return this.saveNew(
+        		test, 
+        		this.buildBuffer(zCrudRequest)
+        );
+	}
+	
+	private StringBuilder buildBuffer(ZCrudRequest zCrudRequest) throws IOException {
+		
+		String json = ObjectMapperProviderForTest.getInstance().get().writerWithDefaultPrettyPrinter().writeValueAsString(zCrudRequest);
+		
+		StringReader reader = new StringReader(json);
+		
+		int intValueOfChar;
+	    StringBuilder buffer = new StringBuilder();
+	    while ((intValueOfChar = reader.read()) != -1) {
+	        buffer.append((char) intValueOfChar);
+	    }
+	    reader.close();
+	    
+		return buffer;
+	}
+	
+	/**/
+	protected String saveNew(String test, StringBuilder buffer) throws IOException {
+		
+		URL resource = getClass().getResource(
+				this.buildResourceString(test)
+		);
+    	File parent = (new File( resource.getFile() ) ).getParentFile().getParentFile();
+    	String newFileName = parent.getPath() + this.buildNewFileName(test);
+		File newFile = new File(newFileName);
+    	newFile.createNewFile();
+    	 
+        this.writeBufferToFile(buffer, newFile);
+        
+        return newFileName;
+	}
+	
+	private void writeBufferToFile(StringBuilder buffer, File file) throws IOException {
+		
+		Writer fileWriter = new FileWriter(file);
+        fileWriter.write(buffer.toString());
+        fileWriter.close();
+	}
+	
+	private String buildNewFileName(String test) {
+		return this.buildResourceString(test) + NEW_FILE_SUFFIX;
+	}
+	
+	private String buildResourceString(String test) {
+		//return "/" + test;
+		return "/requests/" + test + ".json";
+	}
 }
