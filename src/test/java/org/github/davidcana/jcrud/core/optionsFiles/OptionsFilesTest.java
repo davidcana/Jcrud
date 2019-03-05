898e81a1e663e56d96b551da7ddef89589b8a961
David Cana <david.javapagetemplates@gmail.com>
2019-03-05 19:02:21 +0100

    Some duplicated code removed.

--- src/test/java/org/github/davidcana/jcrud/core/optionsFiles/OptionsFilesTest.java
+++ src/test/java/org/github/davidcana/jcrud/core/optionsFiles/OptionsFilesTest.java
@@ -6,13 +6,11 @@ public class OptionsFilesTest extends AbstractOptionsFilesTest {
 
 	@Test
 	public void test() throws Exception {
-		
-        long start = System.currentTimeMillis();
-
-        this.testJavascript("simple", "target/classes/simple.js");
         
-        long elapsed = System.currentTimeMillis() - start;
-        System.err.println("tested in " + elapsed + " ms");
+		this.testJavascript(
+        		"simple", 
+        		"target/classes/simple.js"
+        );
 	}
 
 	
